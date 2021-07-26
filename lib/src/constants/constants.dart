e5ae77c16a6d34889e27f25becbb5c4547f5deae
Hoang <you@example.com>
2021-07-26 10:13:19 +0700

    Comppleted Tablet UI design for Onboarding Screen. And fix fonts for Phone UI

--- lib/src/constants/constants.dart
+++ lib/src/constants/constants.dart
@@ -1,9 +1,19 @@
-class OnboardingPage {}
+class AppOnboardingPage {
+  static const int iconTextColor = 0xff363837;
+  static const int tutorialShareRecipesTextColor = 0xff030F09;
+  static const int tutorialJoinScratchTextColor = 0xffA8A8A8;
+  static const int joinScratchButtonCollor = 0xFF30BE76;
+  static const int leanMoreButtonColor = 0xFFFFFFFF;
+  static const int learMoreButtonBorderColor = 0xFF30BE76;
+  static const int learnMoreButtonTextColor = 0xFF30BE76;
+}
 
 class AppForgotPasswordPage {
+  static const int iconTextColor = 0xFF363837;
   static const int emailTextColor = 0xFFA8A8A8;
   static const int lineInputBorderColor = 0xffCCCCCC;
   static const int buttonColor = 0xff30BE76;
+  static const int emailAddressColor = 0xFF030F09;
 }
 
 class AppConstants {
@@ -13,6 +23,12 @@ class AppConstants {
       "Enter the email associated with your account and we'll send an email with a link to reset your password.";
   static const String emailtext = 'Email address';
   static const String nameButton = 'Send';
+  static const String tutorialShareRecipesText =
+      'Join over 50 millions people sharing recipes everyday';
+  static const String tutorialJoinScratchText =
+      'Never run out of ideas again. Try new foods, ingredients, cooking style, and more';
+  static const String joinScratchButton = 'Join Scratch';
+  static const String learnMoreButton = 'Learn More';
 }
 
 class AppImage {
--- lib/src/screens/forgot_password_page.dart
+++ lib/src/screens/forgot_password_page.dart
@@ -15,94 +15,97 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
     double heightScreen = MediaQuery.of(context).size.height;
     double widthScreen = MediaQuery.of(context).size.width;
     double contextPadding = widthScreen * 0.25 / 3.75;
-    double iconAndTopSpacing = heightScreen * 0.3 / 8.12;
+    double iconAndTopSpacing = heightScreen * 0.6 / 8.12;
     double iconWidth = widthScreen * 0.18 / 3.75;
     double iconHeight = heightScreen * 0.26 / 8.12;
     double iconAndTextSpacing = widthScreen * 0.1 / 3.75;
-    double resetPasswordTextSpacing = heightScreen * 0.45 / 8.12;
+    double resetPasswordTextSpacing = heightScreen * 0.5 / 8.12;
     double tuttorialSpacing = heightScreen * 0.2 / 8.12;
     double emailTextSpacing = heightScreen * 1 / 8.12;
     double enterEmailFieldSpacing = heightScreen * 0.15 / 8.12;
     double buttonSpacing = heightScreen * 0.55 / 8.12;
-    double heightButton = heightScreen * 0.55 / 8.12;
+    double heightButton = heightScreen * 0.5 / 8.12;
 
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: contextPadding),
-          child: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: iconAndTopSpacing),
-                Row(
-                  children: [
-                    Image(
-                        image: AssetImage(AppImage.logoIcon),
-                        width: iconWidth,
-                        height: iconHeight),
-                    SizedBox(width: iconAndTextSpacing),
-                    Text(AppConstants.textIcon,
-                        style:
-                            Theme.of(context).textTheme.headline6?.copyWith()),
-                  ],
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: iconAndTopSpacing),
+              Row(
+                children: [
+                  Image(
+                      image: AssetImage(AppImage.logoIcon),
+                      width: iconWidth,
+                      height: iconHeight),
+                  SizedBox(width: iconAndTextSpacing),
+                  Text(AppConstants.textIcon,
+                      style: Theme.of(context).textTheme.headline6?.copyWith(
+                          fontFamily: "Nunito-Bold",
+                          color: Color(AppForgotPasswordPage.iconTextColor),
+                          letterSpacing: 0.4)),
+                ],
+              ),
+              SizedBox(height: resetPasswordTextSpacing),
+              Text(AppConstants.resetPasswordText,
+                  style: Theme.of(context).textTheme.headline4?.copyWith(
+                      fontFamily: "Nunito-Bold", color: Colors.black)),
+              SizedBox(height: tuttorialSpacing),
+              Text(AppConstants.tutorialText,
+                  style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                        fontFamily: "Nunito-Regular",
+                        color: Colors.black,
+                      )),
+              SizedBox(height: emailTextSpacing),
+              Text(
+                AppConstants.emailtext,
+                style: Theme.of(context).textTheme.subtitle2?.copyWith(
+                    color: Color(
+                      AppForgotPasswordPage.emailTextColor,
+                    ),
+                    fontFamily: "Nunito-Bold"),
+              ),
+              SizedBox(height: enterEmailFieldSpacing),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  isDense: true,
+                  focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color(
+                              AppForgotPasswordPage.lineInputBorderColor))),
                 ),
-                SizedBox(height: resetPasswordTextSpacing),
-                Text(AppConstants.resetPasswordText,
-                    style: Theme.of(context).textTheme.headline4?.copyWith(
-                        fontWeight: FontWeight.bold, color: Colors.black)),
-                SizedBox(height: tuttorialSpacing),
-                Text(AppConstants.tutorialText,
-                    style: Theme.of(context).textTheme.subtitle1),
-                SizedBox(height: emailTextSpacing),
-                Text(
-                  AppConstants.emailtext,
-                  style: Theme.of(context).textTheme.subtitle2?.copyWith(
-                      color: Color(AppForgotPasswordPage.emailTextColor)),
+                style: Theme.of(context).textTheme.subtitle1?.copyWith(
+                    color: Color(AppForgotPasswordPage.emailAddressColor),
+                    fontFamily: "Nunito-Regular"),
+              ),
+              SizedBox(
+                height: buttonSpacing,
+              ),
+              Container(
+                width: widthScreen,
+                height: heightButton,
+                decoration: BoxDecoration(
+                  color: Color(AppForgotPasswordPage.buttonColor),
+                  borderRadius: BorderRadius.circular(8),
                 ),
-                SizedBox(height: enterEmailFieldSpacing),
-                TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  cursorColor: Colors.black,
-                  decoration: InputDecoration(
-                    isDense: true,
-                    focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                            color: Color(
-                                AppForgotPasswordPage.lineInputBorderColor))),
-                  ),
-                  style: Theme.of(context)
-                      .textTheme
-                      .subtitle1
-                      ?.copyWith(color: Colors.black),
-                ),
-                SizedBox(
-                  height: buttonSpacing,
-                ),
-                Container(
-                  width: widthScreen,
-                  height: heightButton,
-                  decoration: BoxDecoration(
-                    color: Color(AppForgotPasswordPage.buttonColor),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Center(
-                      child: Text(
-                        AppConstants.nameButton,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline6
-                            ?.copyWith(color: Colors.white),
-                      ),
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Center(
+                    child: Text(
+                      AppConstants.nameButton,
+                      style: Theme.of(context).textTheme.headline5?.copyWith(
+                          color: Colors.white, fontFamily: "Nunito-Bold"),
                     ),
                   ),
-                )
-              ],
-            ),
+                ),
+              )
+            ],
           ),
         ),
       ),
