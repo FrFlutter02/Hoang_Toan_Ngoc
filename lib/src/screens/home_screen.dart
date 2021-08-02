a2e255306a29710f3e91bcc4f300ed51a78504d3
Hoang <you@example.com>
2021-08-02 17:32:35 +0700

    fix error in review

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class Home_Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Container(
       color: Colors.green,
     );
