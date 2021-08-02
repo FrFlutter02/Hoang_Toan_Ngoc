c1025b84ba55f1c60b67935da4c87d07b4d5d902
Hoang <you@example.com>
2021-08-03 00:33:24 +0700

    onboarding screen for review

---
+++ lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../src/screens/onboarding_screen.dart';
+import '../src/screens/home_screen.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/': (context) => OnboardingScreen(),
+        '/Home': (context) => HomeScreen(),
+      },
+    );
+  }
+}
---
+++ lib/src/constants/constants.dart
@@ -0,0 +1,34 @@
+class AppOnBoardingScreen {
+  static const int iconTextColor = 0xff363837;
+  static const int tutorialShareRecipesTextColor = 0xff030F09;
+  static const int tutorialJoinScratchTextColor = 0xffA8A8A8;
+  static const int joinScratchButtonCollor = 0xFF30BE76;
+  static const int leanMoreButtonColor = 0xFFFFFFFF;
+  static const int learMoreButtonBorderColor = 0xFF30BE76;
+  static const int learnMoreButtonTextColor = 0xFF30BE76;
+}
+
+class AppConstants {
+  static const String textIcon = 'scratch';
+  static const String tutorialShareRecipesText =
+      'Join over 50 millions people sharing recipes everyday';
+  static const String tutorialJoinScratchText =
+      'Never run out of ideas again. Try new foods, ingredients, cooking style, and more';
+  static const String joinScratchButton = 'Join Scratch';
+  static const String learnMoreButton = 'Learn More';
+}
+
+class AppImages {
+  static const String backGroundOnboardingImage =
+      'assets/images/backgroundonboarding_image.png';
+}
+
+class AppIcons {
+  static const String logoIcon = 'assets/icons/logo_icon.png';
+}
+
+class AppFonts {
+  static const String fontAppRegular = "Nunito-Regular";
+  static const String fontAppBold = "Nunito-Bold";
+  static const String fontAppSemiBold = "Nunito-SemiBold";
+}
